de88145882cafc0d1c7035f41591a9d1530ed5fc
Pieter Venter <pieterven12@gmail.com>
2022-10-13 21:26:44 +0200

    [X]

--- Animdo/Animdo/Animdo/Screens/DashboardScreenView.swift
+++ Animdo/Animdo/Animdo/Screens/DashboardScreenView.swift
@@ -9,7 +9,16 @@ import SwiftUI
 
 struct DashboardScreenView: View {
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        VStack{
+  
+            
+            
+            Button(action: {
+               
+            }, label: {
+                Text("Logout")
+            })
+        }
     }
 }
 
